0c9f8a7a3d468618021c0df0ad48f1d20b0c5a5e
Kevin Cheng <kc@kevnull.com>
2017-02-14 00:59:09 -0800

    testing capsule sync

---
+++ Assets/Scripts/ChangeOwner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeOwner : Photon.PunBehaviour {
+
+	public VRTK.VRTK_InteractableObject block;
+
+	// Use this for initialization
+	void Start () {
+		block.InteractableObjectGrabbed += handleObjectGrabbed;
+	}
+
+	void handleObjectGrabbed (object sender, VRTK.InteractableObjectEventArgs e)
+	{
+		block.GetComponent<PhotonView> ().TransferOwnership (PhotonNetwork.player.ID);
+	}
+}
