af0ac938e8695537c6dcf6c512d1a079ce4b1861
Kevin Cheng <kc@kevnull.com>
2017-02-15 20:40:11 -0800

    collision sounds

---
+++ Assets/Scripts/CollisionSound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSound : MonoBehaviour {
+
+    // Use this for initialization
+    private void OnCollisionEnter(Collision collision)
+    {
+        AudioSource aud;
+        aud = GetComponent<AudioSource>();
+        if (!aud.isPlaying)
+            aud.Play();
+    }
+}
